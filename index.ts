9e01057d149c3f4774c4b7a16465c47f2f839d18
Dustin Woods <pyrozzcom@gmail.com>
2019-07-02 15:02:18 -0700

    renamed tempo to click track, fixed index, updated publish files

--- index.ts
+++ index.ts
@@ -1,2 +1,1 @@
 export * from './src/tempo';
-export * from './src/time-signature';
--- src/click-track.ts
+++ src/click-track.ts
@@ -16,7 +16,7 @@ function calculateMeasureBeat(time: number, bpm: number, signature: TimeSignatur
   return beat % signature.beats
 }
 
-export default class Tempo {
+export default class ClickTrack {
   private currentTime: number; // in seconds
   playing: boolean;
   time: BehaviorSubject<number>;
