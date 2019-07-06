21131f0f6facafc85af5edec80ef30b51aceca31
Dustin Woods <pyrozzcom@gmail.com>
2019-07-05 21:22:45 -0700

    Simplified click track, and now using just ste

--- lib/click-track.ts
+++ lib/click-track.ts
@@ -1,19 +1,15 @@
-import { BehaviorSubject, Observable } from "rxjs";
-import { map } from 'rxjs/operators';
-import { DelegatedEventTarget } from "./delegated-event-target";
+import { EventList, IEventHandler } from 'ste-events';
 
-function calculateBeat(time: number, tempo: number): number {
-  return time * (tempo / 60);
+interface Click {
+  beat: number,
+  bar: number,
+  time: number,
+  beatBar: number,
 }
 
-function calculateMeasure(time: number, bpm: number, beats: number): number {
-  const beat = calculateBeat(time, bpm);
-  return beat / beats
-}
-
-function calculateMeasureBeat(time: number, bpm: number, beats: number): number {
-  const beat = calculateBeat(time, bpm);
-  return beat % beats
+interface ClickEvent extends Click {
+  timeDifference: number,
+  beatDifference: number,
 }
 
 type ClickTrackOptions = {
@@ -23,66 +19,140 @@ type ClickTrackOptions = {
   loop?: boolean;
 }
 
-export class ClickTrack extends DelegatedEventTarget {
-  currentTime: number = 0; // in seconds
+type ClickTrackEventName = "beat";// | "bar" | "track" | "start" | "stop";
+
+export class ClickTrack {
   playing: boolean = false;
-  time: BehaviorSubject<number>;
-  beat: Observable<number>;
-  measure: Observable<number>;
-  measureBeat: Observable<number>;
   tempo: number = 0;
   beats: number = 4;
   length: number = Infinity;
   loop: boolean = true;
+  private currentClick: Click;
+  private previousClick: Click;
+  private tempoBPS: number = 0;
+  private events = new EventList<ClickTrack, ClickEvent>();
 
   constructor(options: ClickTrackOptions) {
-    super();
 
     Object.assign(<ClickTrack>this, options);
+
+    // @TODO - update this if this.tempo changes
+    this.tempoBPS = this.tempo / 60;
 
     if(this.tempo === 0 || this.tempo < 0) {
       throw new Error(`Invalid tempo (${this.tempo}), must be greater than 0.`);
     }
 
-    this.time = new BehaviorSubject(0);
+    this.currentClick = {
+      beat: 0,
+      bar: 0,
+      time: 0,
+      beatBar: 0,
+    };
 
-    this.beat = this.time.pipe(
-      map<number, number>((value: number) => {
-        return calculateBeat(value, this.tempo);
-      })
-    );
-
-    this.measure = this.time.pipe(
-      map<number, number>((value: number) => {
-        return calculateMeasure(value, this.tempo, this.beats);
-      })
-    );
-
-    this.measureBeat = this.time.pipe(
-      map<number, number>((value: number) => {
-        return calculateMeasureBeat(value, this.tempo, this.beats);
-      })
-    );
-
-    this.beat.subscribe((beatNumber) => {
-      this.dispatchEvent(new Event('beat'));
-    });
+    this.previousClick = this.currentClick;
 
   }
 
-  setTime(time: number): void {
-    this.currentTime = time;
+  private dispatch(event: ClickTrackEventName, arg: ClickEvent) {
+    this.events.get(event).dispatch(this, arg);
+  }
 
-    if(this.currentTime > this.length) {
+  on(event: ClickTrackEventName, fn: IEventHandler<ClickTrack, ClickEvent>): void {
+    this.events.get(event).subscribe(fn);
+  }
+
+  off(event: ClickTrackEventName, fn: IEventHandler<ClickTrack, ClickEvent>): void {
+    this.events.get(event).unsubscribe(fn);
+  }
+
+  setTime(time: number): void {
+
+    // Handling end of track. Maybe loop to the beginning
+    if(time > this.length) {
       if(!this.loop) {
-        this.currentTime = this.length;
+        time = this.length;
         this.playing = false;
       } else {
-        this.currentTime = this.currentTime % this.length;
+        time = time % this.length;
       }
     }
 
-    this.time.next(this.currentTime);
+    // Set previous click pointer to current click before we change current click
+    this.previousClick = this.currentClick;
+
+    // Calculate current click
+    const beat = time * this.tempoBPS;
+    const bar = beat / this.beats;
+    const beatBar = beat % this.beats;
+    this.currentClick = {
+      time,
+      beat,
+      bar,
+      beatBar,
+    };
+
+    // Get all events that occurred between prior click and current click
+    const clicksBetween = this.getClicksBetween(this.previousClick, this.currentClick);
+
+    // Loop through clicksBetween and dispatch
+    for(let i = 0; i < clicksBetween.length; i++) {
+      this.dispatch("beat", clicksBetween[i]);
+    }
+  }
+
+  private getClicksBetween(fromClick: Click, toClick: Click): Array<ClickEvent> {
+    // 0 -> 5
+    // [0, 1, 2, 3, 4, 5]
+
+    // 0.1 -> 5
+    // [1, 2, 3, 4, 5]
+
+    // 0.1 -> 5.1
+    // [1, 2, 3, 4, 5]
+
+    // 0.1 -> 0.5
+    // []
+
+    // 0.1 -> 1.1
+    // [1]
+
+    // ciel(from) to floor(to)
+
+    // @TODO - check if track has looped on itself
+    if(fromClick.beat > toClick.beat) {
+      throw new Error('Implement calculating clicks for looped track');
+    }
+
+    // Beats are the same
+    if(fromClick.beat === toClick.beat) {
+      return [];
+    }
+
+    const fromBeat = Math.ceil(fromClick.beat);
+    const toBeat = Math.floor(toClick.beat);
+
+    // Beat hasn't advanced an whole number yet
+    if(fromBeat > toBeat) {
+      return [];
+    }
+
+    const clickEvents: Array<ClickEvent> = [];
+
+    for (var i = fromBeat; i <= toBeat; i++) {
+      const time = i / this.tempoBPS;
+      clickEvents.push({
+        time,
+        beat: i,
+        bar: Math.floor(i / this.beats),
+        beatBar: ((i / this.beats) % 1) * this.beats,
+        timeDifference: toClick.time - time,
+        beatDifference: toClick.beat - i,
+      });
+    }
+
+    return clickEvents;
+
   }
 
   tick(delta: number): void {
@@ -90,6 +160,6 @@ export class ClickTrack extends DelegatedEventTarget {
       return;
     }
 
-    this.setTime(this.currentTime + delta);
+    this.setTime(this.currentClick.time + delta);
   }
 }
